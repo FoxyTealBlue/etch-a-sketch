adfb12b8434abc06fcae7a44620b72a88913e5d7
Rina <foxytealblue@gmail.com>
2026-04-21 18:47:00 -0500

    add grid shading and styles

--- main.js
+++ main.js
@@ -2,18 +2,33 @@ console.log("Hello World!");
 const adjustGrid = document.querySelector("#adjustGrid");
 
 adjustGrid.addEventListener("click", () => createDiv(getNumber()));
+createDiv();
 
 // TODO: Use a dialog prompt to change the grid size, and use a input type="range" to set the value.
 
 function createDiv(number = 16) {
   const contentContainer = document.querySelector("#contentContainer");
-  const squareCount = number * number;
+  const cellHeight = `${contentContainer.offsetHeight / number}px`;
+  const cellWidth = `${contentContainer.offsetWidth / number}px`;
   contentContainer.innerHTML = "";
-  for (let i = 0; i < squareCount; i++) {
+  for (let i = 0; i < number * number; i++) {
     const createdDiv = document.createElement("div");
     createdDiv.classList.toggle("cell");
-    createdDiv.style.height = `${contentContainer.offsetHeight / number}px`;
-    createdDiv.style.width = `${contentContainer.offsetWidth / number}px`;
+    createdDiv.style.height = cellHeight;
+    createdDiv.style.width = cellWidth;
+    if (i === 0) {
+      createdDiv.style.borderTopLeftRadius = "10px";
+    }
+    if (i === number - 1) {
+      createdDiv.style.borderTopRightRadius = "10px";
+    }
+    if (i === number ** 2 - number) {
+      createdDiv.style.borderBottomLeftRadius = "10px";
+    }
+    if (i === number ** 2 - 1) {
+      createdDiv.style.borderBottomRightRadius = "10px";
+    }
+    createdDiv.addEventListener("mouseover", () => shadeCell(createdDiv));
     contentContainer.appendChild(createdDiv);
   }
 }
@@ -24,4 +39,8 @@ function getNumber() {
   return number < 2 || number > 100
     ? alert("Incorrect input. Please try again.")
     : number;
+}
+
+function shadeCell(cell) {
+  cell.style.backgroundColor = "black";
 }

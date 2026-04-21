6bbddb2a9d06c7c9107db492d67e18a9ae806811
Rina <foxytealblue@gmail.com>
2026-04-20 19:02:22 -0500

    Add grid

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 console.log("Hello World!");
+const adjustGrid = document.querySelector("#adjustGrid");
+
+adjustGrid.addEventListener("click", () => createDiv(getNumber()));
 
 // TODO: Use a dialog prompt to change the grid size, and use a input type="range" to set the value.
+
+function createDiv(number = 16) {
+  const contentContainer = document.querySelector("#contentContainer");
+  const squareCount = number * number;
+  contentContainer.innerHTML = "";
+  for (let i = 0; i < squareCount; i++) {
+    const createdDiv = document.createElement("div");
+    createdDiv.classList.toggle("cell");
+    createdDiv.style.height = `${contentContainer.offsetHeight / number}px`;
+    createdDiv.style.width = `${contentContainer.offsetWidth / number}px`;
+    contentContainer.appendChild(createdDiv);
+  }
+}
+
+function getNumber() {
+  const number = Number(prompt("Please input a number between 2 and 100."));
+
+  return number < 2 || number > 100
+    ? alert("Incorrect input. Please try again.")
+    : number;
+}
